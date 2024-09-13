15227edf6fe88c952a8e3d898c2fae9b695a0200
Pol Gubau Amores <gubaupol@gmail.com>
2024-09-13 13:15:49 +0200

    Refactor home page to use skeleton loading and display trending movies

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,25 +4,79 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  InfiniteScrollCustomEvent,
+  IonList,
+  IonItem,
+  IonAvatar,
+  IonSkeletonText,
+  IonAlert,
+  IonLabel,
 } from '@ionic/angular/standalone';
 import { MovieService } from '../services/movie.service';
+import { catchError, finalize } from 'rxjs';
+import { MovieResult } from '../services/interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonAvatar,
+    IonSkeletonText,
+    IonAlert,
+    IonLabel,
+  ],
 })
 export class HomePage {
+  private currentPage = 1;
+  public error = null;
+  public isLoading = false;
+  public movies: MovieResult[] = [];
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
   private movieService = inject(MovieService);
+  public skeletonList = new Array(10);
   constructor() {
     this.loadMovies();
   }
 
-  loadMovies() {
-    this.movieService.getTopRatedMovies().subscribe((data) => {
-      console.log(data);
-    });
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+
+    if (!event) {
+      this.isLoading = true;
+    }
+    this.movieService
+      .getTopRatedMovies(this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          if (event) {
+            event.target.complete();
+          }
+        }),
+        catchError((err: any) => {
+          console.log(err);
+          this.error = err.error.status_message;
+          return [];
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.movies.push(...res.results);
+
+          if (event) {
+            event.target.disabled = res.total_pages === this.currentPage;
+          }
+        },
+      });
   }
+  loadMore(event: InfiniteScrollCustomEvent) {}
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { delay, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { ApiResult, MovieResult } from './interfaces';
 
 const API_KEY = environment.apiKey;
 const BASE_URL = 'https://api.themoviedb.org/3';
@@ -13,12 +15,13 @@ export class MovieService {
   private http = inject(HttpClient);
   constructor() {}
 
-  getTopRatedMovies(page = 1) {
-    return this.http.get(
+  getTopRatedMovies(page = 1): Observable<ApiResult> {
+    return this.http.get<ApiResult>(
       `${MOVIE_URL}/popular?page=${page}&api_key=${API_KEY}`
     );
+    // .pipe(delay(1000));
   }
-  getMovieDetails(id: string) {
-    return this.http.get(`${MOVIE_URL}/${id}?api_key=${API_KEY}`);
+  getMovieDetails(id: string): Observable<MovieResult> {
+    return this.http.get<MovieResult>(`${MOVIE_URL}/${id}?api_key=${API_KEY}`);
   }
 }
