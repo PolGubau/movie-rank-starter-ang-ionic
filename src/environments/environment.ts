2596f2ea7c3a706bc0482746448120e4cdb25f2e
Pol Gubau Amores <gubaupol@gmail.com>
2024-09-13 14:16:27 +0200

    Refactor environment.ts to remove access token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,6 @@
 export const environment = {
   production: false,
   apiKey: '5989157e53f0e8d2fd36ebad5d5b5d99',
-  accessToken:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTg5MTU3ZTUzZjBlOGQyZmQzNmViYWQ1ZDViNWQ5OSIsIm5iZiI6MTcyNjIyMzY0NS4wOTAzMDgsInN1YiI6IjY2ZTQwZjVmMDAwMDAwMDAwMGI5OWE3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VU9685xCsVvvix1KfKOoQvffQjmK7OOuaOUgvtb9FD0',
 };
 
 /*
