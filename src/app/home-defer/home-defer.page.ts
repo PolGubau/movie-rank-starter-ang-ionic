d6ff674e990d0f08b37bb34d426a059c705a06c2
Pol Gubau Amores <gubaupol@gmail.com>
2024-09-13 14:27:12 +0200

    Refactor home page to use skeleton loading and display trending movies

--- src/app/home-defer/home-defer.page.ts
+++ src/app/home-defer/home-defer.page.ts
@@ -1,20 +1,95 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { DatePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {
+  InfiniteScrollCustomEvent,
+  IonAlert,
+  IonAvatar,
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonSkeletonText,
+  IonTitle,
+  IonToolbar,
+  IonBadge,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+} from '@ionic/angular/standalone';
+import { catchError, finalize } from 'rxjs';
+import { MovieResult } from '../services/interfaces';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home-defer',
-  templateUrl: './home-defer.page.html',
-  styleUrls: ['./home-defer.page.scss'],
+  templateUrl: 'home-defer.page.html',
+  styleUrls: ['home-defer.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonAvatar,
+    IonSkeletonText,
+    IonAlert,
+    IonLabel,
+    DatePipe,
+    RouterModule,
+    IonBadge,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+  ],
 })
-export class HomeDeferPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeDeferPage {
+  private currentPage = 1;
+  public error = null;
+  public isLoading = false;
+  public movies: MovieResult[] = [];
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+  private movieService = inject(MovieService);
+  public skeletonList = new Array(10);
+  constructor() {
+    this.loadMovies();
   }
 
+  loadMovies(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+
+    if (!event) {
+      this.isLoading = true;
+    }
+    this.movieService
+      .getTopRatedMovies(this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          if (event) {
+            event.target.complete();
+          }
+        }),
+        catchError((err: any) => {
+          console.log(err);
+          this.error = err.error.status_message;
+          return [];
+        })
+      )
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.movies.push(...res.results);
+
+          if (event) {
+            event.target.disabled = res.total_pages === this.currentPage;
+          }
+        },
+      });
+  }
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.currentPage++;
+    this.loadMovies(event);
+  }
 }
